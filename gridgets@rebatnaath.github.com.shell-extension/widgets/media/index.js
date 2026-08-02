2988c73891e1eb81f4fe713afbc5f206d688ca94
rebatnaath <78601591+rebatnaath@users.noreply.github.com>
2026-08-02 18:58:16 +0545

    minor change, moved cities fetching logic for world clock to github instead of local file

--- gridgets@rebatnaath.github.com.shell-extension/widgets/media/index.js
+++
@@ -1,3 +0,0 @@
-export { createStaticImageNode, createColorBlockNode } from './image.js';
-export { createAnimatedGifNode, createAnimatedImageNode } from './gif.js';
-export { createSlideshowNode } from './slideshow.js';
--- gridgets@rebatnaath.github.com.shell-extension/widgets/system/index.js
+++
@@ -1,3 +0,0 @@
-export { createCpuRamNode } from './cpuRam.js';
-export { createNetworkSpeedNode } from './network.js';
-export { createSystemDashboardNode } from './systemDashboard.js';
